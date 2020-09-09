dae39b242b084115a1348ece288dc83def7e0d21
Spyware633 <spyware633@gmail.com>
2020-09-10 00:20:02 +0300

    Add material icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MultiSelectorComponent } from './multiselector/multiselector.component';
 import { MultiLabelComponent } from './multilabel/multilabel.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon'; 
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatIconModule,
+    NoopAnimationsModule
   ],
   declarations: [
     AppComponent,
